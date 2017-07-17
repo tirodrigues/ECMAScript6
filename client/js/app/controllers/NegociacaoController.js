e6ff2a922ba4dbcd5920f23cebbad621947e5bf5
Tiago Rodrigues <tirodrigues.jpa@gmail.com>
2017-07-17 17:03:41 -0300

    ajuste da propiedade quantidade

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,6 +5,7 @@ class NegociacaoController {
     this._inputDate = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
+    this._listaNegociacoes = new ListaNegociacoes();
   }
 
   adiciona(event) {
@@ -12,8 +13,12 @@ class NegociacaoController {
 
     let negociacao = new Negociacao(
         DateHelper.textoParaData(this._inputDate.value),
-        this._inputQuantidade.Value,
+        this._inputQuantidade.value,
         this._inputValor.value
     );
+
+    this._listaNegociacoes.adiciona(negociacao);
+
+    console.log(this._listaNegociacoes.negociacoes);
   }
 }
